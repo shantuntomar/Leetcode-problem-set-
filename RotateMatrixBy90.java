7cb0908dda35ae8e30fa672cd154cdf40aaf15d1
Shantun Tomar <75194520+shantuntomar@users.noreply.github.com>
2022-01-03 10:35:25 +0000

    Create RotateMatrixBy90.java

---
+++ RotateMatrixBy90.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        //transpose 
+        for(int i=0; i<matrix.length; i++) {
+            for(int j=i; j<matrix[0].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        //swap the rows 
+        for(int i=0; i<matrix.length; i++) {
+            int start = 0 , end = matrix[i].length-1;
+            while(start < end) {
+                int temp = matrix[i][start];
+                matrix[i][start] = matrix[i][end];
+                matrix[i][end] = temp;
+                start++;
+                end--;
+            }
+        }
+        
+    }
+}
